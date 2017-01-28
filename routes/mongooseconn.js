0bfbe26fd218d39bdb06431f7594bce62fc15c1b
prasadbalpa <prasad.nala@gmail.com>
2017-01-28 11:14:28 +0530

    Fixed a bug

--- routes/mongooseconn.js
+++ routes/mongooseconn.js
@@ -4,7 +4,7 @@
 var mongoose = require('mongoose');
 var config = require('../app/config/database');
 
-mongoose.connect(config.mongoConnection, function(err) {
+var mongooseconn = mongoose.connect(config.mongoConnection, function(err) {
 	if(err) throw err;
 });
 
