9aa85e7431494f666ac1bbb7ecaa94da3784e262
prasadbalpa <prasad.nala@gmail.com>
2017-01-28 14:01:34 +0530

    Updates to mongoose error handling

--- routes/mongooseconn.js
+++ routes/mongooseconn.js
@@ -3,10 +3,17 @@
  */
 var mongoose = require('mongoose');
 var config = require('../app/config/database');
+try {
+	var mongooseconn = mongoose.connect(config.mongoConnection, function(err) {
+		if(err) {
+			console.log("Error occurred in the connection.....")
+			throw err;
+		}
+	});
+} catch (err ){
+	console.log("Error occured in the connection....handling in the catch")
+}
 
-var mongooseconn = mongoose.connect(config.mongoConnection, function(err) {
-	if(err) throw err;
-});
 
 module.exports = mongooseconn;
 //end of mongoose connection
